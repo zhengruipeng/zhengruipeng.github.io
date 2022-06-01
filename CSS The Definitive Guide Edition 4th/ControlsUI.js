d2e7b8ca4cbacb39543dd78c8d0a66f1d9db441b
zhengruipeng <1484422307@qq.com>
2022-06-01 16:52:45 +0900

    20220601(2)

--- CSS The Definitive Guide Edition 4th/ControlsUI.js
+++ CSS The Definitive Guide Edition 4th/ControlsUI.js
@@ -1,15 +1,31 @@
 let ControlsUI = class extends Object{
     controlsData = {};
     controlsEle = null;
+    cssText = {};
+    initDefaultStyle(url,cssText){
+        fetch(url).then(response => response.text())
+            .then(text => {
+                let style = document.createElement("style");
+                style.innerHTML = text;
+                document.head.appendChild(style);
+            });
+        this.cssText = cssText;
+        return this;
+    }
     initControls(container){
         let fragment = document.createDocumentFragment();
         let header = document.createElement("header");
+        for(let name in this.cssText){
+
+            header.style[name] = this.cssText[name];
+        }
+
         fragment.appendChild(header);
         for (let property in this.controlsData){
             let nav = document.createElement("nav");
             nav.innerHTML = `<p>${property}</p>`;
             // document.createElementNS("http://www.w3.org/2000/svg")
-            if(this.controlsData[property].children){
+            if(this.controlsData[property]?.children){
                 let ul = document.createElement("ul");
                 let temp = document.createElement("div");
                 temp.innerHTML = this.controlsData[property].children;
@@ -41,12 +57,16 @@ let ControlsUI = class extends Object{
         });
         return this;
     }
-    bindItemEvent(events = {}){
+    bindItemEvent(events = {},target){
         let navs = Array.from(this.controlsEle.querySelectorAll("nav"));
         for(let event in events){
             navs.forEach(nav => {
+                let ele = nav;
+                if(target){
+                    ele = nav.querySelector(target);
+                }
                 // nav.addEventListener(event,events[event]);
-                nav.addEventListener(event,function (ev){
+                ele?.addEventListener(event,function (ev){
                     events[event].call(this,ev,this.querySelector("p"),this.querySelector("ul"));
                 });
             });
