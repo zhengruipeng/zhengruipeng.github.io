195d362d2291b4beedc3376347555129df1ddbe7
zhengruipeng <1484422307@qq.com>
2022-12-02 16:37:31 +0900

    20221203

--- mikimiki/APIs/WebRTC/21RTP扩展/js/call/module/addTransceiver.js
+++ mikimiki/APIs/WebRTC/21RTP扩展/js/call/module/addTransceiver.js
@@ -1,24 +1,30 @@
 import {AppGlobal} from "./AppGlobal.js";
 import {ObserverCallBackType} from "../model/ObserverCallBackType.js";
 import {PanelOutput} from "./output-info-into-panel.js"
+import {CallingState} from "../model/CallingState.js";
 
 document.addEventListener("DOMContentLoaded",function (){
-    AppGlobal.beforeAddTrack.addEventListener(ObserverCallBackType.change,function (){
-        let audio = new Audio();
-        audio.src = "../EVERYDAY!%20SUNNYDAY!%20-%20サニーピース.mp3";
-        audio.muted = false;
-        audio.autoplay = true;
-        let audioStream = audio.captureStream();
-        let audioStreamTrack = audioStream.getAudioTracks()[0];
+    AppGlobal.beforeAddTrack.addEventListener(ObserverCallBackType.change,async function (){
+        if(this.value === false)return false;
+
+        let videoStream = await navigator.mediaDevices.getUserMedia({video:true,audio:false});
+        let videoStreamTracks = videoStream.getVideoTracks()[0]
 
         let rtcConn = AppGlobal.RTCConnection;
-        rtcConn.addTransceiver(audioStreamTrack,{
+        rtcConn.addTransceiver(videoStreamTracks,{
             direction: "sendonly",
-            streams:[audioStream],
+            streams:[videoStream],
             sendEncodings:[
-
+                {rid:'q',scaleResolutionDownBy:2,active:true,maxBitrate:44200}
             ]
         });
-        PanelOutput.println("")
+        PanelOutput.println("新しいのコンフィグを適用されています");
+
+    });
+
+    AppGlobal.callingState.addEventListener(ObserverCallBackType.change,function (){
+        if(this.value !== CallingState.END)return false;
+        AppGlobal.beforeAddTrack.value = false;
+
     })
 })
---
+++ mikimiki/APIs/WebRTC/21RTP扩展/js/call/module/getTransceivers.js
@@ -0,0 +1,29 @@
+import {AppGlobal} from "./AppGlobal.js";
+import {ObserverCallBackType} from "../model/ObserverCallBackType.js";
+import {PanelOutput} from "./output-info-into-panel.js"
+import {CallingState} from "../model/CallingState.js";
+
+document.addEventListener("DOMContentLoaded",function (){
+    AppGlobal.callingState.addEventListener(ObserverCallBackType.change,function (){
+        if(this.value !== CallingState.CALLING)return false;
+
+        let rtcConnection = AppGlobal.RTCConnection;
+
+        PanelOutput.println("transceiversを出力する");
+        PanelOutput.println(rtcConnection.getTransceivers().toString2("<br />",-1,"&nbsp;&nbsp;"));
+        console.log(rtcConnection.getTransceivers())
+
+        PanelOutput.println("sendersを出力する");
+        PanelOutput.println("<p style='color:orangered'>"+
+            rtcConnection.getSenders().toString2("<br />",-1,"&nbsp;&nbsp;")+
+        "</p>");
+        console.log(rtcConnection.getSenders())
+
+        PanelOutput.println("receiversを出力する");
+        PanelOutput.println("<p style='color:cadetblue'>"+
+            rtcConnection.getReceivers().toString2("<br />",-1,"&nbsp;&nbsp;")+
+        "</p>");
+        console.log(rtcConnection.getReceivers())
+
+    })
+})
